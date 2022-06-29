dac6f67906bfb53b9537ebeb9fba56782c40db08
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-29 19:32:16 +0430

    Add useRefreshTokens to authProvider

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ ReactDOM.render(
     domain= {process.env.REACT_APP_AUTH0_DOMAIN}
     clientId= {process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
+    useRefreshTokens
+    cacheLocation='memory'
   >
     <App />
   </Auth0Provider>,
